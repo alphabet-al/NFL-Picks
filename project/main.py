90f26c71852b6a04647cf912b79a0a9d0eb36bb2
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2022-09-17 17:08:41 -0500

    final commit

--- project/main.py
+++ project/main.py
@@ -1,9 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import xml
 
 url = 'https://projects.fivethirtyeight.com/2022-nfl-predictions/games/'
 page = urlopen(url)
-# html = page.read().decode('utf-8')
-soup = BeautifulSoup(page, "html.parser")
+soup = BeautifulSoup(page, "lxml")
 
-print(soup.get_text())
+current_week = soup.select('section.week')[0]
+matchups = current_week.find_all(class_ = 'game-body')
+
+winner = []
+
+for i in matchups:
+    versus = i.find_all('td', class_='td text team')
+    probability = i.find_all('td', class_ = 'td number chance')
+    
+    versus = [v.text for v in versus]
+    probability = [p.text for p in probability]
+
+    if probability[0] > probability[1]:
+        winner.append(versus[0])
+    elif probability[1] > probability[0]:
+        winner.append(versus[1])
+    else:
+        winner.append('Even match between {} and {}'.format(versus[0], versus[1]))
+
+print(winner)
